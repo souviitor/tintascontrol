82d6a10e576894c50bde39b85464228dbdbf1dfd
Angelica Pereira Rocha <angelicarocha@MacBook-Air-de-Angelica.local>
2026-04-16 18:30:40 -0300

    fix: app.py

--- app.py
+++ app.py
@@ -864,8 +864,10 @@ def init_db():
         db.session.commit()
         print("✅ Usuário admin criado: admin / admin123")
 
+with app.app_context():
+    init_db()
 
 if __name__ == '__main__':
     with app.app_context():
         init_db()
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5001)))
